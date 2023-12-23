ead5a3ab3967f9c1e2ec85e56894e22e6d6dd39d
huydung179 <huydung179@github.com>
2023-12-23 13:21:22 +0100

    new image + faster inference

--- scripts/demo_video.py
+++ scripts/demo_video.py
@@ -5,17 +5,17 @@ import time
 import warnings
 import argparse
 import cv2
+from matplotlib import pyplot as plt
+from matplotlib.animation import FuncAnimation
 import torch
 import numpy as np
-from pathlib import Path
 from PIL import Image
 from torchvision.models.detection.image_list import ImageList
-from torchvision import transforms as T
 
 from realtime_panoptic.models.rt_pano_net import RTPanoNet
 from realtime_panoptic.config import cfg
 import realtime_panoptic.data.panoptic_transform as P
-from realtime_panoptic.utils.visualization import visualize_segmentation_image, visualize_detection_image
+from realtime_panoptic.utils.visualization import visualize_segmentation_image
 from utils import VideoGet
 
 cityscapes_colormap = np.array([
@@ -43,6 +43,15 @@ cityscapes_colormap = np.array([
 cityscapes_instance_label_name = [
     'person', 'rider', 'car', 'truck', 'bus', 'train', 'motorcycle', 'bicycle']
 warnings.filterwarnings("ignore", category=UserWarning)
+
+
+def wait_for_stream(vid_getter):
+    last_check = time.time()
+    while vid_getter.frame is None:
+        current_time = time.time()
+        if current_time - last_check > 2:
+            print("Waiting for stream...")
+            last_check = current_time
 
 
 def demo():
@@ -91,39 +100,52 @@ def demo():
                    cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
     vid_getter = vid.start()
 
-    count = 0
-    while 1:
-        if (cv2.waitKey(1) == ord('q')):
-            vid_getter.stop()
-            break
-        frame = vid_getter.frame
-        if frame is None:
-            count += 1
-            continue
-        if count == 10:
-            vid_getter.stop()
-            break
+    wait_for_stream(vid_getter)
 
-        cv2.imshow('Frame', frame)
-        input_image = Image.fromarray(frame)
+    plt.figure(figsize=(10, 4))
+    ax1 = plt.subplot(1, 2, 1)
+    ax2 = plt.subplot(1, 2, 2)
+    plt.gca().set_axis_off()
+    plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
+    plt.margins(0, 0)
+    plt.gca().xaxis.set_major_locator(plt.NullLocator())
+    plt.gca().yaxis.set_major_locator(plt.NullLocator())
+
+    # Set no ticks.
+    ax1.set_xticks([])
+    ax1.set_yticks([])
+    ax2.set_xticks([])
+    ax2.set_yticks([])
+
+    frame = vid_getter.frame
+    im1 = ax1.imshow(frame[:, :, ::-1])
+    im2 = ax2.imshow(frame[:, :, ::-1])
+
+    def update(i):
         start = time.time()
 
+        frame = vid_getter.frame
+        input_image = Image.fromarray(frame)
         data = {'image': input_image}
         data = transform(data)
         with torch.no_grad():
             input_image_list = ImageList([data['image'].to(device)], image_sizes=[
-                                         input_image.size[::-1]])
+                input_image.size[::-1]])
             panoptic_result, _ = model.forward(input_image_list)
-            semseg_logics = [
-                o.to('cpu') for o in panoptic_result["semantic_segmentation_result"]]
-            semseg_prob = [torch.argmax(semantic_logit, dim=0)
-                           for semantic_logit in semseg_logics]
+            semseg_logits = panoptic_result["semantic_segmentation_result"][0]
+            semseg_prob = semseg_logits.argmax(dim=0)
 
             seg_vis = visualize_segmentation_image(
-                semseg_prob[0], input_image, cityscapes_colormap)
-            cv2.imshow('Panoptic', seg_vis)
+                semseg_prob, input_image, cityscapes_colormap)
+
+        im1.set_data(frame[:, :, ::-1])
+        im2.set_data(seg_vis.astype('uint8')[:, :, ::-1])
+
         total_time = time.time() - start
         print("Inference time: {:.2f} ms".format(total_time * 1000))
+
+    ani = FuncAnimation(plt.gcf(), update, interval=1)
+    plt.show()
 
 
 if __name__ == "__main__":
